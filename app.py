315c3f521e04efd6263300016249316c776b770f
SahilPadhiyar18 <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-06-16 16:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST']) 
 def data():
     try:
-        
-        client = pymongo.MongoClient("mongodb+srv://SahilCluster18:meHtA9662@sahilcluster.qmaty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-        db = client.test
+        client = pymongo.MongoClient("mongodb+srv://SahilCluster18:meHtA9662@sahilcluster.qmaty.mongodb.net/?retryWrites=true&w=majority")
+#        db = client.test 
+#         client = pymongo.MongoClient("mongodb+srv://SahilCluster18:meHtA9662@sahilcluster.qmaty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+#         db = client.test
         db1 = client["tankdata"]
         dates = client.list_database_names()
         times = client.list_database_names() 
