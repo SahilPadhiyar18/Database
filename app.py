986791b8418cacbc83c52d3f43133a34b78bc490
SahilPadhiyar18 <77140242+SahilPadhiyar18@users.noreply.github.com>
2022-06-22 15:35:06 +0530

    Create app.py

--- app.py
+++ app.py
@@ -13,19 +13,17 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST']) 
 def data():
     try:
-        client = pymongo.MongoClient("mongodb+srv://SahilCluster18:meHtA9662@sahilcluster.qmaty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-
-#         client = pymongo.MongoClient("mongodb+srv://SahilCluster18:meHtA9662@sahilcluster.qmaty.mongodb.net/?retryWrites=true&w=majority")
-#         db = client.test 
+        client = pymongo.MongoClient("mongodb+srv://SahilCluster18:meHtA9662@sahilcluster.qmaty.mongodb.net/?retryWrites=true&w=majority")
+        db = client.test 
 #         client = pymongo.MongoClient("mongodb+srv://SahilCluster18:meHtA9662@sahilcluster.qmaty.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 #         db = client.test
 #         db1 = client["tankdata"]
 #         dates = client.list_database_names()
 #         times = client.list_database_names() 
 #         state = client.list_database_names() 
-        return "Done"
+        return "adasd"
     except:
-        return "exe"
+        return "Try agaim"
 
 @app.route('/espupdate', methods=['GET','POST'])
 def espupdate():
